5c8e140b83fdff8231ecee3d874604c03857adea
THECODE-MASTER <63726176+THECODE-MASTER@users.noreply.github.com>
2020-05-10 13:24:58 +0530

    Stick Castle

--- BaseClass.js
+++ BaseClass.js
@@ -1,8 +1,8 @@
 class BaseClass{
-    constructor(x, y, width, height, angle) {
+    constructor(x, y, width, height) {
         var options = {
-            'restitution':0.8,
-            'friction':1.0,
+            'restitution':0,
+            'friction':10000000000000000000000000000000000000000000000000000000000000000,
             'density':1.0
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
